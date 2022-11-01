41edcaa9d9f2470c39486ec8fa231c4b320749ae
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-02 00:01:37 +0300

    Add template

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,9 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index() -> str:
     user = {'username': 'Alsiri0n'}
-    return '''
-    <html>
-        <head>
-            <title>Home page - Microblof</title>
-        </head>
-        <body>
-            <h1>Hello, ''' + user['username'] + '''!</h1>
-        </body>
-    </html>'''
+    return render_template('index.html', title='Home', user=user)
