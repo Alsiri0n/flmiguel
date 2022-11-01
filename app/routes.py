ab7252bba0f8af4924fb850a1befc04837097dc7
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-01 23:40:14 +0300

    Add username to index page

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,13 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index() -> str:
-    return "Hello World!"
+    user = {'username': 'Alsiri0n'}
+    return '''
+    <html>
+        <head>
+            <title>Home page - Microblof</title>
+        </head>
+        <body>
+            <h1>Hello, ''' + user['username'] + '''!</h1>
+        </body>
+    </html>'''
