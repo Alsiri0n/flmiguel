464630828c85e4001c355406755b0dc6dc63a567
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-02 00:08:58 +0300

    Add posts loop at template

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,14 @@ from app import app
 @app.route('/index')
 def index() -> str:
     user = {'username': 'Alsiri0n'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in World!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
