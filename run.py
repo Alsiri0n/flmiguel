10f1faad21a547df4bcb6291b115ee7187a4e16b
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-03 12:30:09 +0300

    Add secretkey

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import os
 from app import app
 
-FLASK_HOST = str(os.environ.get('flask_host'))
-FLASK_PORT = int(os.environ.get('flask_port'))
 
 if __name__ == "__main__":
     app.run(host=FLASK_HOST, port=FLASK_PORT)
