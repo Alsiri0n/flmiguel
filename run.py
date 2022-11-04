dbbafde727a1866e329014ed18ccdd82101e3857
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-04 11:41:39 +0300

    Trying fix start

--- run.py
+++ run.py
@@ -2,9 +2,6 @@ import os
 from app import app
 from config import Config
 
-FLASK_HOST = str(os.environ.get('flask_host'))
-FLASK_PORT = int(os.environ.get('flask_port'))
-
 
 if __name__ == "__main__":
-    app.run(host=FLASK_HOST, port=FLASK_PORT)
+    app.run(host=Config.FLASK_HOST, port=Config.FLASK_PORT)
