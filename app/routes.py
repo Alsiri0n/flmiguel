3e9f09d66411e1b5e39b1424808e87d78a703f3e
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-05 22:51:46 +0300

    Restore changes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 """
 Module for routing into application
 """
-# from werkzeug.urls import url_parse
+from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app
@@ -44,10 +44,10 @@ def login():
             flash('Invalid username or password')
             return(redirect(url_for('login')))
         login_user(user, remember=form.remember_me.data)
-        # next_page = request.args.get('next')
-        # if not next_page or url_parse(next_page).netloc != '':
-        #     next_page = url_for('index')
-        # return redirect(next_page)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign in', form=form)
 
 @app.route('/logout')
