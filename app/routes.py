ddbdbab9de77046d1cf47f463bfa6f88945d44e4
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-06 20:28:01 +0300

    Add profile page

--- app/routes.py
+++ app/routes.py
@@ -75,3 +75,15 @@ def register():
         flash('Congratulations, you are now a registred user!')
         return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username: str):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
