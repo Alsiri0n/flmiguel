8c2656d71954bd51e7d0cba88ed1be68af239862
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-07 22:51:52 +0300

    Add error handler SMTP and File

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ class Config(object):
         or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT') or 465)
+    MAIL_PORT = os.environ.get('MAIL_PORT') or 465
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['ADMIN_MAIL']
+    ADMINS = [os.environ.get('ADMIN_MAIL')]
