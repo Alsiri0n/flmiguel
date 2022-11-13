de65413d9c9375129843ed0643b662d99e66f351
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-13 18:13:00 +0300

    Add translate for moments.js

--- app/routes.py
+++ app/routes.py
@@ -3,14 +3,14 @@ Module for routing into application
 """
 from datetime import datetime
 from werkzeug.urls import url_parse
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
 from flask_login import current_user, login_user, logout_user, login_required
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm, \
  ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User, Post
 from app.email import send_password_reset_email
-from flask_babel import _
+from flask_babel import _, get_locale
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -226,6 +226,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.locale = str(get_locale())
 
 
 # @app.template_filter('format_datetime')
