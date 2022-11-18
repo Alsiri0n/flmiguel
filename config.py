8d82d38e6e5a6cd27608327ec1ea72a098c9148f
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-18 14:28:13 +0300

    Add logging to stdout

--- config.py
+++ config.py
@@ -27,3 +27,4 @@ class Config(object):
     LANGUAGES = ['en', 'ru']
     NLP_TRANSLATOR_KEY = os.environ.get('NLP_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOT_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
