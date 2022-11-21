177bd6e52fb151faa67a3fef9e84bbb6abe13bf6
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-21 17:35:16 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -3,4 +3,4 @@ source venv/bin/activate
 # flask db upgrade
 flask translate compile
 exec gunicorn -b :5050 --access-logfile - --error-logfile - run:app
-venv/bin/rq worker -u $REDIS_URL microblog-tasks
+exec venv/bin/rq worker -u $REDIS_URL microblog-tasks
