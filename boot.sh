910304cedff56649796bc0bdff73c8460536bda4
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-21 22:56:49 +0300

    Remove old string from boot

--- boot.sh
+++ boot.sh
@@ -4,4 +4,3 @@ source venv/bin/activate
 # flask db upgrade
 flask translate compile
 exec gunicorn -b :$flask_port --access-logfile - --error-logfile - run:app
-# exec rq worker -u $REDIS_URL/5 microblog-tasks
