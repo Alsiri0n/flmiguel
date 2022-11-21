3a8fa32eb966c02072d507a539b69b9f9099dca5
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-21 17:54:57 +0300

    Change docker compose

--- boot.sh
+++ boot.sh
@@ -3,4 +3,5 @@ source venv/bin/activate
 # flask db upgrade
 flask translate compile
 exec gunicorn -b :5050 --access-logfile - --error-logfile - run:app
+echo $REDIS_URL
 exec venv/bin/rq worker -u $REDIS_URL microblog-tasks
