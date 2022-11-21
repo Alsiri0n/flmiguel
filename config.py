d124f5507a5d26e0c79cf2988aacb973dd07ef2e
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-11-21 17:41:09 +0300

    Update config

--- config.py
+++ config.py
@@ -28,4 +28,4 @@ class Config(object):
     NLP_TRANSLATOR_KEY = os.environ.get('NLP_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
-    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
+    REDIS_URL = os.environ.get('REDIS_URL') # or 'redis://'
